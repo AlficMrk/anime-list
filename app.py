4d6907d4bd92f74e160b61f732c33748b1a3792d
AlficMrk <astraldural@gmail.com>
2026-02-10 10:23:11 +0500

    Update app.py
    Rer

--- app.py
+++ app.py
@@ -220,5 +220,5 @@ def init_db():
 # Запуск приложения
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
